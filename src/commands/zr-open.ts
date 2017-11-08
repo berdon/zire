3dff59824bdb53ab637bbcb506761b2aac9b1f0a
Austin Hanson <austin.hanson@zuerchertech.com>
2017-11-08 14:35:36 -0600

    Adding zr-open command to open issues in the browser

---
+++ src/commands/zr-open.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+/*** Node Modules ***/
+let sprintf = require("sprintfjs");
+
+/*** Typed Modules ***/
+import chalk from 'chalk';
+import preferences from 'preferences';
+import * as opn from 'opn';
+
+/*** Local Modules ***/
+import { Command } from "../interfaces";
+import AbstractCommand from './abstractCommand';
+
+export class OpenCommand extends AbstractCommand {
+    async ShowHelp(argOptions: any, ...args: string[]): Promise<number> {
+        return 0;
+    }
+
+    async Run(options : any, ...args: string[]): Promise<number> {
+        let issue = await this.jira.findIssue(args[0]);
+        let link = sprintf('https://jira.zuerchertech.com/browse/%s', issue.key);
+
+        console.log('Opening %s', link)
+        opn(sprintf('https://jira.zuerchertech.com/browse/%s', issue.key));
+
+        return 0;
+    }
+}
+
+export default (new OpenCommand());
